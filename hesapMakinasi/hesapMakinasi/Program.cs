8efc63a6335d8724dab00209799a8e80ec840587
mirkelam32 <mirkelam1299@gmail.com>
2020-05-17 04:15:16 +0300

    Ödevler_17.05.2020

---
+++ hesapMakinasi/hesapMakinasi/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hesapMakinasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double sayi1, sayi2;
+            string islemAdi;
+
+            Console.WriteLine(" Birinci sayı: " );
+            sayi1 = int.Parse(Console.ReadLine());
+            Console.WriteLine(" İkinci sayı: " );
+            sayi2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" İşlemler: Toplamak: (+) Çıkartmak: (-) Çarpmak: (*) Bölmek: (/)");
+            islemAdi = Console.ReadLine();
+
+            if (islemAdi == "+")
+            {
+                Console.WriteLine("İşlemin sonucu:" + sayi1 + " + " + sayi2 + " = " + (sayi1 + sayi2));
+            }
+           else if (islemAdi == "-")
+            {
+                Console.WriteLine("İşlemin sonucu:" + sayi1 + " - " + sayi2 + " = " + (sayi1 - sayi2));
+            }
+          else if (islemAdi == "*")
+            {
+                Console.WriteLine("İşlemin sonucu:" + sayi1 + " * " + sayi2 + " = " + (sayi1 * sayi2));
+            }
+           else if (islemAdi == "/")
+            {
+                Console.WriteLine("İşlemin sonucu:" + sayi1 + " / " + sayi2 + " = " + (sayi1 / sayi2));
+            }
+            else
+            {
+                Console.WriteLine("Hatalı işlem yaptınız..!");
+            }
+            Console.Read();
+
+        }
+        
+        
+    }
+}
---
+++ hesapMakinasi2/hesapMakinasi2/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hesapMakinasi2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sayi1, sayi2, sayi3;
+            string birinciIslem, ikinciIslem;
+
+            Console.WriteLine("Birinci sayıyı yazınız: (Tam Sayı) Örnek: 3");
+            sayi1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("İkinci sayıyı yazınız: (Tam Sayı) Örnek: 7");
+            sayi2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Üçüncü sayıyı yazınız: (Tam Sayı) Örnek: 13");
+            sayi3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Birinci ve ikinci işlemleri seçiniz: Toplamak ( + ) Çıkartmak ( - ) Bölmek ( / ) Çarpmak ( * )");
+            Console.WriteLine("birinci işlemi yazınız.");
+            birinciIslem = Console.ReadLine();
+            Console.WriteLine("ikinci işlemi yazınız.");
+            ikinciIslem = Console.ReadLine();
+
+
+            if (birinciIslem == "%" && ikinciIslem == "%")
+            {
+                Console.WriteLine("Sonuc: " + birinciIslem + " fark " + ikinciIslem + " fark " + (sayi1 % sayi2 % sayi3));
+            }
+            if (birinciIslem == "+" && ikinciIslem == "+")
+            {
+                Console.WriteLine(" Sonuc: " + sayi1 + " + " + sayi2 + " + " + sayi3 + " = " + (sayi1 + sayi2 + sayi3));
+            }
+            else if (birinciIslem == "-" && ikinciIslem == "-")
+            {
+                Console.WriteLine(" Sonuc: " + sayi1 + " - " + sayi2 + " - " + sayi3 + " = " + (sayi1 - sayi2 - sayi3));
+            }
+            else if (birinciIslem == "/" && ikinciIslem == "/")
+            {
+                Console.WriteLine(" Sonuc: " + sayi1 + " / " + sayi2 + " / " + sayi3 + " = " + (sayi1 / sayi2 / sayi3));
+            }
+            else if (birinciIslem == "*" && ikinciIslem == "*")
+            {
+                Console.WriteLine(" Sonuc: " + sayi1 + " * " + sayi2 + " * " + sayi3 + " = " + (sayi1 * sayi2 * sayi3));
+            }
+
+            if (birinciIslem == "+" && ikinciIslem == "-")
+            {
+                Console.WriteLine(" Sonuc: " + sayi1 + " + " + sayi2 + " - " + sayi3 + " = " + (sayi1 + sayi2 - sayi3));
+            }
+            else if (birinciIslem == "+" && ikinciIslem == "/")
+            {
+                Console.WriteLine(" Sonuc: " + sayi1 + " + " + sayi2 + " / " + sayi3 + " = " + (sayi1 + sayi2 / sayi3));
+            }
+            else if (birinciIslem == "+" && ikinciIslem == "*")
+            {
+                Console.WriteLine(" Sonuc: " + sayi1 + " + " + sayi2 + " * " + sayi3 + " = " + (sayi1 + sayi2 * sayi3));
+            }
+
+            if (birinciIslem == "-" && ikinciIslem == "+")
+            {
+                Console.WriteLine(" Sonuc: " + sayi1 + " - " + sayi2 + " + " + sayi3 + " = " + (sayi1 - sayi2 + sayi3));
+            }
+            else if (birinciIslem == "-" && ikinciIslem == "/")
+            {
+                Console.WriteLine(" Sonuc: " + sayi1 + " - " + sayi2 + " / " + sayi3 + " = " + (sayi1 - sayi2 / sayi3));
+            }
+            else if (birinciIslem == "-" && ikinciIslem == "*")
+            {
+                Console.WriteLine(" Sonuc: " + sayi1 + " - " + sayi2 + " * " + sayi3 + " = " + (sayi1 - sayi2 * sayi3));
+            }
+
+            if (birinciIslem == "/" && ikinciIslem == "+")
+            {
+                Console.WriteLine(" Sonuc: " + sayi1 + " / " + sayi2 + " + " + sayi3 + " = " + (sayi1 / sayi2 + sayi3));
+            }
+            else if (birinciIslem == "/" && ikinciIslem == "-")
+            {
+                Console.WriteLine(" Sonuc: " + sayi1 + " / " + sayi2 + " - " + sayi3 + " = " + (sayi1 / sayi2 - sayi3));
+            }
+            else if (birinciIslem == "/" && ikinciIslem == "*")
+            {
+                Console.WriteLine(" Sonuc: " + sayi1 + " / " + sayi2 + " * " + sayi3 + " = " + (sayi1 / sayi2 * sayi3));
+            }
+
+            if (birinciIslem == "*" && ikinciIslem == "+")
+            {
+                Console.WriteLine(" Sonuc: " + sayi1 + " * " + sayi2 + " + " + sayi3 + " = " + (sayi1 * sayi2 + sayi3));
+            }
+            else if (birinciIslem == "*" && ikinciIslem == "-")
+            {
+                Console.WriteLine(" Sonuc: " + sayi1 + " * " + sayi2 + " - " + sayi3 + " = " + (sayi1 * sayi2 - sayi3));
+            }
+            else if (birinciIslem == "+" && ikinciIslem == "*")
+            {
+                Console.WriteLine(" Sonuc: " + sayi1 + " * " + sayi2 + " / " + sayi3 + " = " + (sayi1 * sayi2 / sayi3));
+            }else
+            {
+                Console.WriteLine("Hatalı işlem yaptınız.!");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
